283f5b86582d6322ce087955dac8c87a0d506531
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-04-01 23:28:25 +0800

    Add delete endpoint in the AccountController

--- HowsYourDayApi/Controllers/AccountController.cs
+++ HowsYourDayApi/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using HowsYourDayApi.DTOs.Authentication;
+using HowsYourDayApi.Extensions;
 using HowsYourDayApi.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace HowsYourDayApi.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
+        private readonly IDayEntryService _dayEntryService;
 
-        public AccountController(IAccountService accountService)
+        public AccountController(IAccountService accountService, IDayEntryService dayEntryService)
         {
             _accountService = accountService;
+            _dayEntryService = dayEntryService;
         }
 
         [HttpPost("register")]
@@ -48,6 +51,27 @@ namespace HowsYourDayApi.Controllers
         {
             await _accountService.LogoutAsync(HttpContext);
             return Ok("User logged out successfully");
+        }
+        
+        [HttpDelete("data")]
+        [Authorize]
+        public async Task<IActionResult> Delete()
+        {
+            var userId = User.GetUserId();
+            
+            // Get current user
+            var user = await _accountService.GetUserAsync(userId);
+
+            if (user == null)
+                return NotFound();
+            
+            // Delete user
+            await _accountService.DeleteUserAsync(user);
+                
+            // Delete all day entries of user
+            await _dayEntryService.DeleteDayEntriesOfUserAsync(userId);
+            
+            return Ok();
         }
     }
 }
