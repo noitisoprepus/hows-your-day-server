e8d09206639aaee21971dcbad544aee1a5b6284c
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-04-05 22:01:38 +0800

    Make UserId property of DayEntry non-nullable

--- HowsYourDayApi/Models/DayEntry.cs
+++ HowsYourDayApi/Models/DayEntry.cs
@@ -3,7 +3,7 @@ namespace HowsYourDayApi.Models
     public class DayEntry
     {
         public Guid Id { get; set; }
-        public Guid? UserId { get; set; }
+        public Guid UserId { get; set; }
         public DateTime LoggedAtUtc { get; set; }
         public int Rating { get; set; }
         public string Note { get; set; }
