e456a1a5e8b1168cadcaeb49914c4812504b6fb0
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-04-06 23:59:33 +0800

    Add DbSet for DayEntryAnalysis in the DbContext

--- HowsYourDayApi/Data/HowsYourDayAppDbContext.cs
+++ HowsYourDayApi/Data/HowsYourDayAppDbContext.cs
@@ -12,6 +12,7 @@ namespace HowsYourDayApi.Data
         }
 
         public DbSet<DayEntry> DayEntries { get; set; }
+        public DbSet<DayEntryAnalysis> DayEntryAnalyses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
